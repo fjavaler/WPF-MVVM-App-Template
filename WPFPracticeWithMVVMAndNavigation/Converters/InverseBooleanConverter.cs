13776ee8c430ffda4406eb2d76297e9652c001a0
Frederick Javalera <frederickjavalera@gmail.com>
2024-02-27 19:09:05 -0700

    Adding Converters example and README.md

---
+++ WPFPracticeWithMVVMAndNavigation/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfAppTemplate.Converters
+{
+  /// <summary>
+  /// A converter class that inverts the boolean value of a property. 
+  /// </summary>
+  public class InverseBooleanConverter : IValueConverter
+  {
+    /// <summary>
+    /// Conversion method required for IValueConverter implementation.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <param name="targetType">The target's class type.</param>
+    /// <param name="parameter">Parameter.</param>
+    /// <param name="culture">I'm not sure but is unused.</param>
+    /// <returns>Inverted boolean value.</returns>
+    /// <exception cref="ArgumentException">Thrown if value is not a bool.</exception>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      bool isBool = value is bool;
+      if (isBool)
+      {
+        return !(bool)value;
+      }
+      else
+      {
+        throw new ArgumentException($"{value} must be of type bool.");
+      }
+    }
+
+    /// <summary>
+    /// Converts value back to original type.
+    /// </summary>
+    /// <param name="value">The value to convert back.</param>
+    /// <param name="targetType">The target's class type.</param>
+    /// <param name="parameter">Parameter.</param>
+    /// <param name="culture">Culture info.</param>
+    /// <returns>Re-inverted boolean value.</returns>
+    /// <exception cref="NotImplementedException">Not implemented (not needed).</exception>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ WPFPracticeWithMVVMAndNavigation/README.md
@@ -0,0 +1,18 @@
+﻿# WpfAppTemplate
+
+WPF app template targeting .NET 8.
+
+## How to use this template.
+1. Click on the use template button on the Repo's home page.
+2. Name the new app that you will be creating.
+3. Clone the repo for the new app locally.
+4. `CTRL+SHIFT+F` somewhere in the code if in VS 2022 and search for WpfAppTemplate.
+5. Click on the 'Replace in Files' tab.
+6. In the replace text box enter the name of the new app.
+7. Click on the 'Replace All' button.
+8. Verify project builds and runs.
+9. Rename the Main Window's Title attribute to the name of the app.
+10. Rename the project to name of new app.
+11. Rename the solution to name of new app.
+12. Search for and rename all references to "WpfAppTemplate" with the chosen name for the Application.
+*If things aren't working correctly (like bindings, commands, etc.) verify that all references to "WpfAppTemplate" have been removed.*
