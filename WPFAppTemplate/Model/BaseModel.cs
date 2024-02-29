0bb62559b90b62e3e8197fdd259263a94a7f2a3c
Frederick Javalera <frederickjavalera@gmail.com>
2024-02-28 17:47:28 -0700

    Adding SetProperty method.

--- WPFAppTemplate/Model/BaseModel.cs
+++ WPFAppTemplate/Model/BaseModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WPFAppTemplate.Model
 {
@@ -12,6 +13,27 @@ namespace WPFAppTemplate.Model
     #endregion
 
     #region Methods
+    /// <summary>
+    /// Sets field to newValue for property, propertyName, and raises
+    /// property changed event.
+    /// </summary>
+    /// <typeparam name="T">Generic type.</typeparam>
+    /// <param name="field">field to update.</param>
+    /// <param name="newValue">The new value.</param>
+    /// <param name="propertyName">The name of the property being updated.</param>
+    /// <returns>True if setting of property succeeded.</returns>
+    public bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+    {
+      if (EqualityComparer<T>.Default.Equals(field, newValue))
+      {
+        return false;
+      }
+
+      field = newValue;
+      OnPropertyChanged(propertyName);
+      return true;
+    }
+
     /// <summary>
     /// Raises the PropertyChanged event.
     /// </summary>
