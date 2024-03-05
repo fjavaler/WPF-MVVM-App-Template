0f82b33d3d617b0e56d07a0eebfc2160cdcc1142
Frederick Javalera <frederickjavalera@gmail.com>
2024-03-04 18:19:38 -0700

    Adding ISoundService and SoundService

---
+++ WPFAppTemplate/Service/Abstract/ISoundService.cs
@@ -0,0 +1,22 @@
+﻿namespace WPFAppTemplate.Service.Abstract
+{
+  public interface ISoundService
+  {
+    /// <summary>
+    /// Play a sound.
+    /// </summary>
+    /// <param name="fileName">Name of the file.</param>
+    public void PlaySound(string fileName);
+
+    /// <summary>
+    /// Play a looping sound.
+    /// </summary>
+    /// <param name="fileName"></param>
+    public void PlayLoopingSound(string fileName);
+
+    /// <summary>
+    /// Stop the sound.
+    /// </summary>
+    public void StopSound();
+  }
+}
---
+++ WPFAppTemplate/Service/Concrete/SoundService.cs
@@ -0,0 +1,69 @@
+﻿using System.Media;
+using WPFAppTemplate.Service.Abstract;
+
+namespace WPFAppTemplate.Service.Concrete
+{
+  public class SoundService : ISoundService
+  {
+    /// <summary>
+    /// Sound player.
+    /// </summary>
+    public SoundPlayer Player { get; set; } = new SoundPlayer();
+
+    /// <inheritdoc/>
+    public void PlaySound(string fileName)
+    {
+      using (Player)
+      {
+        try
+        {
+          Player.SoundLocation = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"../../../Audio/{fileName}");
+          Player.Load();
+          Player.Play();
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+    }
+
+    /// <inheritdoc/>
+    public void PlayLoopingSound(string fileName)
+    {
+      using (Player)
+      {
+        try
+        {
+          Player.SoundLocation = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"../../../Audio/{fileName}");
+          Player.Load();
+          Player.PlayLooping();
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+    }
+
+    /// <inheritdoc/>
+    public void StopSound()
+    {
+      using (Player)
+      {
+        Player.Stop();
+        Player.Dispose();
+      }
+    }
+
+    /// <summary>
+    /// Destructor for the SoundService.
+    /// </summary>
+    ~SoundService()
+    {
+      // Dispose of the player in order to avoid memory leaks.
+      Player.Stop();
+      Player.Dispose();
+    }
+  }
+}
